96ccfd8cb766ee86c87fdb8495d4eb1ee1219da5
希嘉嘉 <hi@xijaja.com>
2023-05-25 22:11:10 +0800

    add readme

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Go-Vercel
+
+将 gofiber 框架的后端程序部署到 vercel 上，尝试访问：
+- GET [/](https://go-vercel-phi.vercel.app/)
+- GET [/v1](https://go-vercel-phi.vercel.app/v1)
+- GET [/v2](https://go-vercel-phi.vercel.app/v2)
--- api/index.go
+++ api/index.go
@@ -6,15 +6,15 @@ import (
 	"net/http"
 )
 
-// Handler is the main entry point of the application. Think of it like the main() method
+// Handler 是应用程序的主要入口点。可以将其视为 main() 方法
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// This is needed to set the proper request path in `*fiber.Ctx`
+	// 需要在 `*fiber.Ctx` 中设置正确的请求路径
 	r.RequestURI = r.URL.String()
-
+	// 通过适配器将 fiber 应用程序转换为 http.Handler
 	handler().ServeHTTP(w, r)
 }
 
-// building the fiber application
+// 构建 fiber 应用程序
 func handler() http.HandlerFunc {
 	app := fiber.New()
 
@@ -32,8 +32,8 @@ func handler() http.HandlerFunc {
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
-			"uri":  ctx.Request().URI().String(),
-			"path": ctx.Path(),
+			"uri":  ctx.Request().URI().String(), // 获取请求域名
+			"path": ctx.Path(),                   // 获取请求路径
 		})
 	})
 
